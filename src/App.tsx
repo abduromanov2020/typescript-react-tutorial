165a8fe70b8399d9ef3300cdd4641cdc86e0cff5
abduromanov2020 <abduromanov2020@gmail.com>
2023-03-13 01:01:26 +0700

    feature: add list components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Section from "./components/Section";
 import "./App.css";
 import { useState } from "react";
 import Counter from "./components/Counter";
+import List from "./components/List";
 
 function App() {
   const [count, setCount] = useState(1);
@@ -12,6 +13,10 @@ function App() {
       <Heading title="Hello" />
       <Section>This is MySection</Section>
       <Counter setCount={setCount}>Count is {count}</Counter>
+      <List
+        items={["Coffee", "Tacos", "Code"]}
+        render={(item: string) => <span className="gold">{item}</span>}
+      />
     </>
   );
 }
