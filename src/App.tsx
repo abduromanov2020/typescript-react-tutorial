7da46595a6936af15d08d2b8910b624c45158d8a
abduromanov2020 <abduromanov2020@gmail.com>
2023-03-13 00:42:54 +0700

    feature: add counter components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Heading from "./components/Heading";
 import Section from "./components/Section";
 import "./App.css";
+import { useState } from "react";
+import Counter from "./components/Counter";
 
 function App() {
+  const [count, setCount] = useState(1);
+
   return (
     <>
       <Heading title="Hello" />
       <Section>This is MySection</Section>
+      <Counter setCount={setCount}>Count is {count}</Counter>
     </>
   );
 }
